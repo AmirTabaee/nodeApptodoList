bae83ea2db2db7416680691b61f7982b8e07ad7d
Amir Alavi <sir.amir.71@gmail.com>
2021-10-26 01:18:41 +0330

    you can now add , remove or set to complete a todo

--- model/todo.js
+++ model/todo.js
@@ -5,10 +5,10 @@ const rootDir = require("../utils/path");
 const filepath = path.join(rootDir, "data", "todos.json");
 
 class Todo {
-    constructor(id, text, complted = true) {
+    constructor(id, text, completed = false) {
         this.id = id;
         this.text = text;
-        this.complted = complted;
+        this.completed = completed;
     }
 
     save(callback) {
@@ -23,12 +23,37 @@ class Todo {
         });
     }
 
-    static fetchAll(callback){
-        fs.readFile( filepath , ( err , fileContent) => {
-            if(err) return([]);
+    static fetchAll(callback) {
+        fs.readFile(filepath, (err, fileContent) => {
+            if (err) return [];
             const todos = JSON.parse(fileContent);
             callback(todos);
-        })
+        });
+    }
+
+    static deleteTodo(id, callback) {
+        fs.readFile(filepath, (err, fileContent) => {
+            const todos = JSON.parse(fileContent);
+            const filteredTodos = todos.filter((t) => t.id != id);
+
+            fs.writeFile(filepath, JSON.stringify(filteredTodos), (err) => {
+                callback();
+            });
+        });
+    }
+
+    static setTodoToComplete(id, callback) {
+        fs.readFile(filepath, (err, fileContent) => {
+            const todos = JSON.parse(fileContent);
+            const todoIndex = todos.findIndex((t) => t.id == id);
+            const todo = todos[todoIndex];
+            todo.completed = true;
+            todos[todoIndex] = todo;
+
+            fs.writeFile(filepath, JSON.stringify(todos), (err) => {
+                callback(err);
+            });
+        });
     }
 }
 
