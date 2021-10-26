a32476d524ac85b652ab89ed772e8ada872846d5
Amir Alavi <sir.amir.71@gmail.com>
2021-10-26 04:55:03 +0330

    uuid package installed for genereting uniqe ids

--- utils/todos.js
+++ utils/todos.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const {v4: uuidv4} = require('uuid');
+
 const rootDir = require("./path");
 const filePath = path.join(rootDir, "data", "todos.json");
 
@@ -17,7 +19,7 @@ exports.saveTodos = (todos, callback) => {
 };
 
 exports.generateRandomId = () => {
-    return Math.floor(Math.random() * 1000);
+    return uuidv4();
 };
 
 exports.getCompletedTodos = (callback) => {
