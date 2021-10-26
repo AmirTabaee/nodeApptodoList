32042921df1ffc58f25070fff4b91f21fbf59343
Amir Alavi <sir.amir.71@gmail.com>
2021-10-26 04:35:48 +0330

    remaining and completed todos now are shown on the main page

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,10 +1,17 @@
 const Todo = require("../model/todo");
+const { getCompletedTodos, getRemainingTodos } = require("../utils/todos");
 
 exports.todoIndex = (req, res) => {
-    Todo.fetchAll((todos) => {
-        res.render("index", {
-            pageTitl: "نمایش کارهای روزمره",
-            todos,
+    getCompletedTodos((completedTodos) => {
+        getRemainingTodos((remainingTodos) => {
+            Todo.fetchAll((todos) => {
+                res.render("index", {
+                    pageTitl: "نمایش کارهای روزمره",
+                    todos,
+                    completedTodos,
+                    remainingTodos,
+                });
+            });
         });
     });
 };
